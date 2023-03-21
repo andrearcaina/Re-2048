d9ae00774ab1207c1972a52e20ae80d6049844f4
Andre <dtandre331@gmail.com>
2023-03-21 09:35:47 -0400

    implemented tile colours
    - i thought i fixed it
    i didn't... :(

--- assets/game.js
+++ assets/game.js
@@ -1,11 +1,15 @@
 let board; let score = 0;      //these variables can be changed
 const rows = 4; const cols = 4; //these cannot be changed
 
+// onload, call setupBoard()
+windows.onload = function() {
+    setupBoard();
+}
+
 /* 
     set up the game board for 2048
     basically a matrix or a 4x4 array
 */
-
 function setupBoard() { 
     board =    [[0,0,0,0],
                 [0,0,0,0],
@@ -15,10 +19,11 @@ function setupBoard() {
     for(let row = 0; row < rows; i++){
         for(let col = 0; col < cols; j++){
             let gameTile = document.createElement("div"); 
-            board = document.querySelector(".gameBoard");
+            let number = board[row][col];
+            const BOARD = document.getElementById("gameBoard");
 
             gameTile.id = row.toString() + "-" + col.toString();
-            updateTiles(gameTile); board.append(gameTile);
+            updateTiles(gameTile, number); BOARD.append(gameTile);
         }
     }
     // setup two tiles at beginning after setup board
@@ -29,8 +34,17 @@ function setupBoard() {
     this function can be changed later on
     when the user presses input keys
 */
-function updateTiles(gameTile) {
+function updateTiles(gameTile, number) {
     gameTile.innerText = "";
     gameTile.classList.value = ""; 
     gameTile.classList.add("gameTile");
+    if (number > 0) {
+        gameTile.innerText = number;
+    }
+    else if (number <= 4096) {
+        gameTile.classList.add("t"+number.toString());
+    }
+    else {
+        gameTile.classList.add("t8192");
+    }
 }
