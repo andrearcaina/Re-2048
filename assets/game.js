a5814a1e9546bdd4fd3ff24a48893b016943875b
Andre <dtandre331@gmail.com>
2023-03-21 13:05:04 -0400

    FIXED THE ISSUE
    - windows.onload -> window.onload
    - for loop still had i++ and j++, had to switch to row++ and col++
    - this was the main problem
    - added a function to find if there is an empty tile
    - if there is an empty tile, add two random "2" blocks at a random row and a random column
    - added some basic comments to the code
    
    Todo:
    - Implement functionality with key event listeners such as WASD or the arrow keys
    - add animation when sliding the blocks
    - implement swiping function alongside mobile input using Hammer js

--- assets/game.js
+++ assets/game.js
@@ -1,23 +1,18 @@
-let board; let score = 0;      //these variables can be changed
-const rows = 4; const cols = 4; //these cannot be changed
-
-// onload, call setupBoard()
-windows.onload = function() {
-    setupBoard();
-}
+let board; let score = 0;           //these variables can be changed 
+const ROWS = 4; const COLS = 4;     //these cannot be changed
 
 /* 
     set up the game board for 2048
     basically a matrix or a 4x4 array
 */
-function setupBoard() { 
+window.onload = function() {
     board =    [[0,0,0,0],
                 [0,0,0,0],
                 [0,0,0,0],             
                 [0,0,0,0]
                 ]
-    for(let row = 0; row < rows; i++){
-        for(let col = 0; col < cols; j++){
+    for(let row = 0; row < ROWS; row++){
+        for(let col = 0; col < COLS; col++){
             let gameTile = document.createElement("div"); 
             let number = board[row][col];
             const BOARD = document.getElementById("gameBoard");
@@ -28,23 +23,56 @@ function setupBoard() {
     }
     // setup two tiles at beginning after setup board
     // this can be implemented later on (once I figure out how to add tiles)
+    blockTwo();
+    blockTwo();
 }
 
-/* 
-    this function can be changed later on
-    when the user presses input keys
-*/
 function updateTiles(gameTile, number) {
     gameTile.innerText = "";
     gameTile.classList.value = ""; 
     gameTile.classList.add("gameTile");
     if (number > 0) {
         gameTile.innerText = number;
+        if (number <= 4096) {
+            gameTile.classList.add("t"+number.toString());
+        }
+        else {
+            gameTile.classList.add("t8192");
+        }
     }
-    else if (number <= 4096) {
-        gameTile.classList.add("t"+number.toString());
+}
+
+function blockTwo() {
+    let emptyTile = false;
+    let tileFound = false;
+
+    //checks if there is an empty tile
+    for (let row = 0; row < ROWS; row++) {
+        for (let col = 0; col < COLS; col++) {
+            if (board[row][col] == 0) { 
+                emptyTile = true;
+            }
+        }
     }
-    else {
-        gameTile.classList.add("t8192");
+
+    // if there is an empty tile, then return nothing
+    if (!emptyTile) { return; }
+    
+    while (!tileFound) {
+        /*
+            reason to use Math.floor
+            Math.random() returns a random number between (0 and 1)*4
+            resulting in a decimal number between 0 and 4
+            thus, Math.floor returns an integer value of the highest decimal number of that decimal
+        */
+        let row = Math.floor(Math.random() * ROWS);
+        let col = Math.floor(Math.random() * COLS);
+        if (board[row][col] == 0) {
+            board[row][col] = 2;
+            let gameTile_ID = document.getElementById(row.toString() + "-" + col.toString());
+            gameTile_ID.innerText = "2";
+            gameTile_ID.classList.add("t2");
+            tileFound = true;
+        }
     }
 }
