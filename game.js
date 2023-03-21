5738aafa0c9ecb24eaacfbcf163a816830cb2bac
Andre <dtandre331@gmail.com>
2023-03-20 21:57:08 -0400

    Added necessary implementations
    - implemented index.html
    - implemented styles.css
    - created basic gameboard and html layout
    - started a bit on gameplay side
    - yet to: setup board and add tiles
    - code css of tiles
    - code functionality of arrow keys (and wasd)
    - code swiping mechanism using Hammer js

---
+++ game.js
@@ -0,0 +1,17 @@
+let board; let score = 0;      //these variables can be changed
+const rows = 4; const col = 4; //these cannot be changed
+
+function setupBoard() { 
+    board =    [[0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0],             
+                [0,0,0,0]
+                ]
+    for(let i = 0; i < rows; i++){
+        for(let j = 0; j < col; j++){
+            
+        }
+    }
+    // setup two tiles at beginning
+
+}
